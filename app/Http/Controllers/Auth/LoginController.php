ebe5a723b908da8fda2bb37057dc632ad626b8e4
ryuki1126 <uv.ryuki1126@gmail.com>
2022-03-25 23:20:22 +0900

    first commit

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+use App\Models\User;
+use \Symfony\Component\HttpFoundation\Response;
+
+class LoginController extends Controller
+{
+    public function login(Request $request){
+        // バリデーション
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        // ログイン処理
+        if(Auth::attempt($credentials)){
+            $user = User::whereEmail($request->email)->first(); //トークン作成と取得
+
+            $user->tokens()->delete();
+            $token = $user->createToken("login:user{$user->id}")->plainTextToken;
+            
+            //ログインが成功するとtokenを返す。
+            return response()->json(['token' => $token], Response::HTTP_OK);
+        }
+        // ログイン失敗時の処理
+        return response()->json('Can Not Login.', Response::HTTP_INTERNAL_SERVER_ERROR);
+    }
+
+    public function logout(Request $request){
+        auth('sanctum')->user()->tokens()->delete();
+        return response(['message' => 'You have been successfully logged out.'], 200);
+    }
+
+    public function user(Request $request){
+        $user = $request->user();
+        return response()->json(compact('user'), 200);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\LoginController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('/test', function(){
+    return 'api is working!';
+});
+
+Route::post('/register', [RegisterController::class, 'register']);
+Route::post('/login', [LoginController::class, 'login']);
+Route::post('/logout', [LoginController::class, 'logout']);
+
+Route::middleware('auth:sanctum')->group(function(){
+    Route::get('/user',[LoginController::class,'user']);
+    Route::get('/hoge', function(){
+        return 'auth is working!!';
+    });
+});
